eb8b07abc83627d1c683121582f354676fef4fe7
Keichi Takahashi <keichi.t@me.com>
2018-06-04 19:42:53 +0900

    Started implementing the evaluator

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,61 @@
 mod lexer;
 mod parser;
-
 use lexer::Lexer;
 use parser::{Parser, Value};
 
-fn main() {
-    let lexer = Lexer::new("((lambda (x) x) 1)");
-    let mut parser = Parser::new(lexer);
+fn add(args: &[Value]) -> Result<Value, String> {
+    let mut result = 0;
 
-    println!("{:?}", parser.parse());
+    for v in args.into_iter() {
+        match eval(v)? {
+            Value::Integer(i) => result += i,
+            _ => return Err("Cannot add non-integer".to_owned())
+        }
+    }
+
+    Ok(Value::Integer(result))
+}
+
+fn apply(f: &Value, args: &[Value]) -> Result<Value, String> {
+    match f {
+        Value::Symbol(s) => match s.as_str() {
+            "+" => add(args),
+            "-" => add(args),
+            "*" => add(args),
+            "/" => add(args),
+            s => Err(format!("Unbound variable {}", s))
+        },
+        v => Err(format!("Invalid application to {}", v))
+    }
+}
+
+fn eval(value: &Value) -> Result<Value, String> {
+    match value {
+        Value::List(ref vs) => {
+            if vs.is_empty() {
+                return Ok(Value::List(Vec::new()))
+            }
+
+            return apply(&vs[0], &vs[1..]);
+        },
+        Value::DottedList(_) => Err("Cannot evaluate dotted list".to_owned()),
+        &Value::Boolean(b) => Ok(Value::Boolean(b.clone())),
+        &Value::Symbol(ref s) => Ok(Value::Symbol(s.clone())),
+        &Value::Integer(i) => Ok(Value::Integer(i.clone())),
+        &Value::String(ref s) => Ok(Value::String(s.clone()))
+    }
+}
+
+fn main() {
+    let lexer = Lexer::new("(+ 1 2 3)");
+    let mut parser = Parser::new(lexer);
+    let parsed = parser.parse();
+
+    match parsed {
+        Ok(v) => match eval(&v) {
+            Ok(v) => println!("{}", v),
+            Err(e) => println!("{:?}", e)
+        },
+        Err(e) => println!("{:?}", e)
+    }
 }

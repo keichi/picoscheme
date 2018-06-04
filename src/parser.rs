dc3c8b76d055afc298aea2aa88088652b3af983e
Keichi Takahashi <keichi.t@me.com>
2018-06-04 17:26:43 +0900

    Use Vec to represent lists

--- src/parser.rs
+++ src/parser.rs
@@ -1,11 +1,12 @@
 use std::iter::{Iterator,Peekable};
+use std::vec::Vec;
 
 use lexer::Token;
 
 #[derive(Debug,PartialEq)]
 pub enum Value {
-    Pair(Box<Value>, Box<Value>),
-    Nil,
+    List(Vec<Value>),
+    DottedList(Vec<Value>),
     Boolean(bool),
     Symbol(String),
     Integer(i64),
@@ -35,21 +36,26 @@ impl<T: Iterator<Item=Token>> Parser<T> {
     }
 
     fn parse_list(&mut self) -> Result<Value, String> {
-        match self.tokens.peek().cloned() {
-            Some(Token::CloseParen) => {
-                self.tokens.next();
-                Ok(Value::Nil)
-            },
-            Some(Token::Dot) => {
-                self.tokens.next();
-                Ok(self.parse()?)
-            },
-            Some(_) => {
-                let car = Box::new(self.parse()?);
-                let cdr = Box::new(self.parse_list()?);
-                Ok(Value::Pair(car, cdr))
-            },
-            None => Err("Unexepcted end of list".to_owned())
+        self.tokens.next();
+
+        let mut items = Vec::new();
+
+        loop {
+            match self.tokens.peek().cloned() {
+                Some(Token::CloseParen) => {
+                    self.tokens.next();
+                    return Ok(Value::List(items));
+                },
+                Some(Token::Dot) => {
+                    self.tokens.next();
+                    items.push(self.parse()?);
+                    return Ok(Value::DottedList(items));
+                },
+                Some(_) => {
+                    items.push(self.parse()?);
+                },
+                None => return Err("Unexepcted end of list".to_owned())
+            }
         }
     }
 
@@ -74,43 +80,36 @@ impl<T: Iterator<Item=Token>> Parser<T> {
                 self.tokens.next();
                 Ok(Value::Symbol(s))
             },
-            Token::OpenParen => {
-                self.tokens.next();
-                self.parse_list()
-            },
+            Token::OpenParen => self.parse_list(),
             Token::Quote => {
                 self.tokens.next();
                 self.parse().map(|v|
-                    Value::Pair(
-                        Box::new(Value::Symbol("quote".to_owned())),
-                        Box::new(v)
+                    Value::List(
+                        vec![Value::Symbol("quote".to_owned()), v]
                     )
                 )
             },
             Token::BackQuote => {
                 self.tokens.next();
                 self.parse().map(|v|
-                    Value::Pair(
-                        Box::new(Value::Symbol("quasiquote".to_owned())),
-                        Box::new(v)
+                    Value::List(
+                        vec![Value::Symbol("quasiquote".to_owned()), v]
                     )
                 )
             },
             Token::Comma => {
                 self.tokens.next();
                 self.parse().map(|v|
-                    Value::Pair(
-                        Box::new(Value::Symbol("unquote".to_owned())),
-                        Box::new(v)
+                    Value::List(
+                        vec![Value::Symbol("unquote".to_owned()), v]
                     )
                 )
             },
             Token::CommaAt => {
                 self.tokens.next();
                 self.parse().map(|v|
-                    Value::Pair(
-                        Box::new(Value::Symbol("unqote-splicing".to_owned())),
-                        Box::new(v)
+                    Value::List(
+                        vec![Value::Symbol("unquote-splicing".to_owned()), v]
                     )
                 )
             }
@@ -144,7 +143,7 @@ fn test_parse_literal() {
 }
 
 #[test]
-fn test_proper_list() {
+fn test_list() {
     let tokens = vec![
         Token::OpenParen,
         Token::String("a".to_owned()),
@@ -155,22 +154,44 @@ fn test_proper_list() {
     let mut parser = Parser::new(tokens.into_iter());
 
     assert_eq!(
-        Value::Pair(
-            Box::new(Value::String("a".to_owned())),
-            Box::new(Value::Pair(
-                Box::new(Value::Integer(123)),
-                Box::new(Value::Pair(
-                    Box::new(Value::Symbol("b".to_owned())),
-                    Box::new(Value::Nil)
-                )),
-            ))
-        ),
+        Value::List(vec![
+            Value::String("a".to_owned()),
+            Value::Integer(123),
+            Value::Symbol("b".to_owned())
+        ]),
         parser.parse().unwrap()
     );
 }
 
 #[test]
-fn test_improper_list() {
+fn test_nested_list() {
+    let tokens = vec![
+        Token::OpenParen,
+        Token::String("a".to_owned()),
+        Token::Integer(123),
+        Token::OpenParen,
+        Token::Identifier("b".to_owned()),
+        Token::Integer(456),
+        Token::CloseParen,
+        Token::CloseParen
+    ];
+    let mut parser = Parser::new(tokens.into_iter());
+
+    assert_eq!(
+        Value::List(vec![
+            Value::String("a".to_owned()),
+            Value::Integer(123),
+            Value::List(vec![
+                Value::Symbol("b".to_owned()),
+                Value::Integer(456),
+            ]),
+        ]),
+        parser.parse().unwrap()
+    );
+}
+
+#[test]
+fn test_dotted_list() {
     let tokens = vec![
         Token::OpenParen,
         Token::Integer(123),
@@ -181,8 +202,10 @@ fn test_improper_list() {
     let mut parser = Parser::new(tokens.into_iter());
 
     assert_eq!(
-        Value::Pair(Box::new(Value::Integer(123)),
-                    Box::new(Value::Integer(456))),
+        Value::DottedList(vec![
+            Value::Integer(123),
+            Value::Integer(456)
+        ]),
         parser.parse().unwrap()
     );
 }

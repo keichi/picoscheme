e724b69700c222dd61413383423e2a6cd1231bab
Keichi Takahashi <keichi.t@me.com>
2018-06-05 13:17:16 +0900

    Refactor evaluation of list

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ fn add(args: &[Value]) -> Result<Value, String> {
     let mut result = 0;
 
     for v in args.iter() {
-        match &eval(v)? {
+        match v {
             &Value::Integer(i) => result += i,
             v => return Err(format!("Cannot apply non-integer {} to +", v))
         }
@@ -20,7 +20,7 @@ fn sub(args: &[Value]) -> Result<Value, String> {
     if args.is_empty() {
         return Err("- requires at least one argument".to_owned())
     } else if args.len() == 1 {
-        match &eval(&args[0])? {
+        match &args[0] {
             &Value::Integer(i) => return Ok(Value::Integer(-i)),
             v => return Err(format!("Cannot apply non-integer {} to -", v))
         }
@@ -28,13 +28,13 @@ fn sub(args: &[Value]) -> Result<Value, String> {
 
     let mut result: i64;
 
-    match &eval(&args[0])? {
+    match &args[0] {
         &Value::Integer(i) => result = i,
         v => return Err(format!("Cannot apply non-integer {} to -", v))
     }
 
     for v in &args[1..] {
-        match &eval(v)? {
+        match v {
             &Value::Integer(i) => result -= i,
             v => return Err(format!("Cannot apply non-integer {} to -", v))
         }
@@ -47,7 +47,7 @@ fn mul(args: &[Value]) -> Result<Value, String> {
     let mut result = 1;
 
     for v in args.iter() {
-        match &eval(v)? {
+        match v {
             &Value::Integer(i) => result *= i,
             v => return Err(format!("Cannot apply non-integer {} to *", v))
         }
@@ -61,7 +61,7 @@ fn car(args: &[Value]) -> Result<Value, String> {
         return Err("car requires 1 argument".to_owned())
     }
 
-    match &eval(&args[0])? {
+    match &args[0] {
         &Value::List(ref vs) => Ok(vs[0].clone()),
         &Value::DottedList(ref vs) => Ok(vs[0].clone()),
         _ => Err("car requires list".to_owned())
@@ -73,7 +73,7 @@ fn cdr(args: &[Value]) -> Result<Value, String> {
         return Err("cdr requires 1 argument".to_owned())
     }
 
-    match &eval(&args[0])? {
+    match &args[0] {
         &Value::List(ref vs) if vs.is_empty() =>
             Err("Cannot take cdr of empty list".to_owned()),
         &Value::List(ref vs) => Ok(Value::List(vs[1..].to_vec())),
@@ -96,35 +96,41 @@ fn cons(args: &[Value]) -> Result<Value, String> {
         return Err("cons requires 2 arguments".to_owned())
     }
 
-    let car = eval(&args[0])?;
-    let cdr = eval(&args[1])?;
+    let car = &args[0];
+    let cdr = &args[1];
 
-    match &cdr {
+    match cdr {
         &Value::List(ref vs) => {
-            let mut list = vec![car];
-            list.extend_from_slice(&vs);
+            let mut list = vec![car.clone()];
+            list.extend_from_slice(&vs[..]);
             return Ok(Value::List(list));
         },
         &Value::DottedList(ref vs) => {
-            let mut list = vec![car];
+            let mut list = vec![car.clone()];
             list.extend_from_slice(&vs);
             return Ok(Value::DottedList(list));
         },
-        v => Ok(Value::DottedList(vec![car, v.clone()]))
+        v => Ok(Value::DottedList(vec![car.clone(), v.clone()]))
     }
 }
 
 fn apply(f: &Value, args: &[Value]) -> Result<Value, String> {
-    match f {
+    match &eval(f)? {
         &Value::Symbol(ref s) => match s.as_str() {
-            "+" => add(args),
-            "-" => sub(args),
-            "*" => mul(args),
-            "car" => car(args),
-            "cdr" => cdr(args),
-            "cons" => cons(args),
             "quote" => quote(args),
-            s => Err(format!("Unbound variable {}", s))
+            _ => {
+                let args: Vec<_> = try!(args.iter().map(eval).collect());
+
+                match s.as_str() {
+                    "+" => add(&args[..]),
+                    "-" => sub(&args[..]),
+                    "*" => mul(&args[..]),
+                    "car" => car(&args[..]),
+                    "cdr" => cdr(&args[..]),
+                    "cons" => cons(&args[..]),
+                    s => Err(format!("Unbound variable {}", s))
+                }
+            },
         },
         v => Err(format!("Invalid application to {}", v))
     }

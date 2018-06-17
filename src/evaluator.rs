73ba8e6a799b536efaeabc7be142e16c1c55e853
Keichi Takahashi <keichi.t@me.com>
2018-06-17 17:37:31 +0900

    Implemented let and letrec form

--- src/evaluator.rs
+++ src/evaluator.rs
@@ -105,13 +105,74 @@ fn if_exp(args: &[Value], env: Rc<Environment>) -> Result<Value, String> {
     }
 
     match eval(&args[0], env.clone())? {
-        Value::Boolean(false) => if args.len() == 2 {
-            Ok(Value::List(Vec::new()))
-        } else {
-            eval(&args[2], env)
-        },
+        Value::Boolean(false) if args.len() == 2 => Ok(Value::List(Vec::new())),
+        Value::Boolean(false) => eval(&args[2], env),
         _ => eval(&args[1], env)
     }
+}
+
+fn let_exp(args: &[Value], env: Rc<Environment>) -> Result<Value, String> {
+    if args.len() < 2 {
+        return Err("let requires more than 2 arguments".to_owned())
+    }
+
+    let new_env = Rc::new(Environment::new_child(env.clone()));
+
+    match &args[0]  {
+        Value::List(ref vs) =>
+            for v in vs {
+                if let Value::List(ref vs) = v {
+                    if vs.len() != 2 {
+                        return Err("let form has malformed binding".to_owned())
+                    }
+
+                    if let Value::Symbol(ref s) = vs[0] {
+                        new_env.define(s, &eval(&vs[1], env.clone())?);
+                    } else {
+                        return Err("let form has malformed binding".to_owned())
+                    }
+                } else {
+                    return Err("let form has malformed binding".to_owned())
+                }
+            },
+        _ => return Err("let form has malformed binding".to_owned())
+    }
+
+    args[1..].iter()
+        .fold(Err("let form has empty body".to_owned()),
+              |_, v| eval(v, new_env.clone()))
+}
+
+fn letrec_exp(args: &[Value], env: Rc<Environment>) -> Result<Value, String> {
+    if args.len() < 2 {
+        return Err("letrec requires more than 2 arguments".to_owned())
+    }
+
+    let new_env = Rc::new(Environment::new_child(env.clone()));
+
+    match &args[0]  {
+        Value::List(ref vs) =>
+            for v in vs {
+                if let Value::List(ref vs) = v {
+                    if vs.len() != 2 {
+                        return Err("letrec form has malformed binding".to_owned())
+                    }
+
+                    if let Value::Symbol(ref s) = vs[0] {
+                        new_env.define(s, &eval(&vs[1], new_env.clone())?);
+                    } else {
+                        return Err("letrec form has malformed binding".to_owned())
+                    }
+                } else {
+                    return Err("letrec form has malformed binding".to_owned())
+                }
+            },
+        _ => return Err("letrec form has malformed binding".to_owned())
+    }
+
+    args[1..].iter()
+        .fold(Err("letrec form has empty body".to_owned()),
+              |_, v| eval(v, new_env.clone()))
 }
 
 fn lambda_exp(args: &[Value], env: Rc<Environment>) -> Result<Value, String> {
@@ -208,6 +269,8 @@ fn eval_list(vs: &[Value], env: Rc<Environment>) -> Result<Value, String> {
         &Value::Symbol(ref s) => match s.as_str() {
             "quote" => quote_exp(args, env),
             "quasiquote" => quasiquote_exp(args, env),
+            "let" => let_exp(args, env),
+            "letrec" => letrec_exp(args, env),
             "if" => if_exp(args, env),
             "lambda" => lambda_exp(args, env),
             "define" => define_exp(args, env),
@@ -248,6 +311,8 @@ pub fn eval(value: &Value, env: Rc<Environment>) -> Result<Value, String> {
             "unquote" => Ok(value.clone()),
             "unquote-splicing" => Ok(value.clone()),
             "if" => Ok(value.clone()),
+            "let" => Ok(value.clone()),
+            "letrec" => Ok(value.clone()),
             "lambda" => Ok(value.clone()),
             "define" => Ok(value.clone()),
             "set!" => Ok(value.clone()),
@@ -305,6 +370,22 @@ mod tests {
             assert_eq!(interp.rep_str(input), expected);
         }
     }
+
+    #[test]
+    fn test_let() {
+        let cases = vec![
+            ("(let ((x 2) (y 3)) (+ x y))",             "5"),
+            ("(let ((x 2) (y 3)) (+ x y) (* x y))",     "6"),
+            ("(let () (+ 1 (* 2 3)))",                  "7")
+        ];
+
+        let interp = Interpreter::new();
+
+        for (input, expected) in cases {
+            assert_eq!(interp.rep_str(input), expected);
+        }
+    }
+
 
     #[test]
     fn test_if() {

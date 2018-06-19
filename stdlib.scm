a7f23b157347b4178a1cc74059f56f0d4794e66c
Keichi Takahashi <keichi.t@me.com>
2018-06-19 17:13:39 +0900

    Added some procedures to stdlib

--- stdlib.scm
+++ stdlib.scm
@@ -1,2 +1,12 @@
 (define list (lambda x x))
+(define null? (lambda (x) (eqv? x '())))
+(define length (lambda (x) (if (null? x) 0 (+ 1 (length (cdr x))))))
+
 (define zero? (lambda (n) (= n 0)))
+(define positive? (lambda (n) (> n 0)))
+(define negative? (lambda (n) (< n 0)))
+
+(define caar (lambda (x) (car (car x))))
+(define cadr (lambda (x) (car (cdr x))))
+(define cdar (lambda (x) (cdr (car x))))
+(define cddr (lambda (x) (cdr (cdr x))))
